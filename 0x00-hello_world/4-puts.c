e28bd5faf3df3bfe03d2e13219e657a241af0ae8
NnannaOgbu <nnannaogbuwa@gmail.com>
2023-03-19 12:20:59 -0700

    Script added

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main - using puts function to pnt a text
+*return: always 0 (succes)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
