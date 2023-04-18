231fd01952dd7f8d3113e35787102ef93dad8c8b
NnannaOgbu <nnannaogbuwa@gmail.com>
2023-04-18 15:16:46 -0700

    Script added

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a struct for a puppy
+ * @a: name
+ * @b: age
+ * @c: owner
+ *
+ * Description: This struct keeps record of the name, age and owners of a collection of puppies
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
