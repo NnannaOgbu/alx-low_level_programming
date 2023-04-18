e0c7404e94c1e6e1544c7464aea052aa7b680b92
NnannaOgbu <nnannaogbuwa@gmail.com>
2023-04-18 06:03:16 -0700

    Script added

--- 0x0D-preprocessor/02-main.c
+++ 0x0D-preprocessor/02-main.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
+/**
+ * main - check the code
+ * __FILE__ returns file name
+ *
+ * Return: Always 0.
+ */
 
 int main(void)
 {
 
-	printf(" %s\n", __FILE__);
+	printf("%s\n", __FILE__);
 
 	return (0);
 }
