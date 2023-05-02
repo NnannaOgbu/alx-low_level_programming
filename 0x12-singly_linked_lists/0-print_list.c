87a72752df8e514d271ca22f7bd885547f27b628
NnannaOgbu <nnannaogbuwa@gmail.com>
2023-05-02 15:23:27 -0700

    Script adjusted

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,14 +9,14 @@
  */
 size_t print_list(const list_t *h)
 {
-    size_t count = 0;
+	size_t count = 0;
 
-    while (h != NULL)
-    {
-        printf("[%d] %s\n", h->len, (h->str == NULL ? "(nil)" : h->str));
-        h = h->next;
-        count++;
-    }
+	while (h != NULL)
+	{
+		printf("[%d] %s\n", h->len, (h->str == NULL ? "(nil)" : h->str));
+		h = h->next;
+		count++;
+	}
 
-    return count;
+	return count;
 }
