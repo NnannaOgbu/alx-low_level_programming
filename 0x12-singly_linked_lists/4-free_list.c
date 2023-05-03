721bcdbe9cc4878788c7604d56135301df26491b
NnannaOgbu <nnannaogbuwa@gmail.com>
2023-05-03 04:30:05 -0700

    Script to free malloc added

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+/**
+ * free_list- function that frees a list_t list.
+ * @head: this is a pointer to the head of the list
+ *
+ * Return: Always 0
+ */
+
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp->str);
+		free(tmp);
+	}
+}
