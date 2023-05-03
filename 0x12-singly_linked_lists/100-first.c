aaedf514a4409ae008c7370c76d87ebc139699a1
NnannaOgbu <nnannaogbuwa@gmail.com>
2023-05-03 05:02:25 -0700

    Script added

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void beforeMain()
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
---
+++ 0x12-singly_linked_lists/100-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void beforeMain();
+
+int main(void)
+{
+	beforeMain();
+
+	printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
+	return (0);
+}
