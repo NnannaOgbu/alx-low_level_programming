8a63cc5bf05ad1b418f7a4059dc5119a7adac7c1
NnannaOgbu <nnannaogbuwa@gmail.com>
2023-05-05 18:14:33 -0700

    Script adjusted

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -17,11 +17,11 @@ unsigned int binary_to_uint(const char *b)
 	if (b == NULL)
 		return (0);
 
-	for (int i = 0; b[i] != '\0'; i++)
+	for (; *b; b++)
 	{
-		if (b[i] == '0')
+		if (*b == '0')
 			result = result << 1;
-		else if (b[i] == '1')
+		else if (*b == '1')
 			result = (result << 1) | 1;
 		else
 			return (0);
